48a81f9485544fefab7facc3e6b84f1116a5522c
L0RD3N <97124425+L0RD3N@users.noreply.github.com>
2022-10-04 09:04:54 +0300

    Add files via upload

--- Everytime we eat we get teleported/src/main/java/handlers/EatingTeleport.java
+++ Everytime we eat we get teleported/src/main/java/handlers/EatingTeleport.java
@@ -3,6 +3,7 @@ package handlers ;
 import maria.plugin_maria.Plugin_Maria;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,9 +23,22 @@ public class EatingTeleport implements Listener {
 
         Player player = playerItemConsumeEvent.getPlayer();
         Random random = new Random() ;
-        double destinationX = random.nextDouble(5000);
-        double destinationY = random.nextDouble(64, 256) ;
-        double destinationZ = random.nextDouble(5000);
+        double destinationX, destinationY, destinationZ ;
+
+        if ((player.getWorld().getEnvironment().equals(World.Environment.NETHER)) || (player.getWorld().getEnvironment().equals(World.Environment.NETHER)))
+        {
+
+            destinationX = random.nextDouble(100);
+            destinationY = random.nextDouble(64, 20) ;
+            destinationZ = random.nextDouble(100);
+
+        }
+
+        else {
+            destinationX = random.nextDouble(2000);
+            destinationY = random.nextDouble(64, 256);
+            destinationZ = random.nextDouble(2000);
+        }
 
         Location destination = new org.bukkit.Location(player.getWorld(), destinationX, destinationY, destinationZ) ;
         for(Player player1 : Bukkit.getServer().getOnlinePlayers()) {
